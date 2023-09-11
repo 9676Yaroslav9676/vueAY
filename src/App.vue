f8d7f2d3d4f43036f76f6a89ab26102b42abad2f
9676Yaroslav9676 <yaroslavrokitskyi@gmail.com>
2023-09-11 18:43:26 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,52 @@
 <template>
-  <div>
-    <div class="post" v-for="post in posts" :key="post.id">
-      <div><strong>Назва: </strong>{{ post.title }}</div>
-      <div><strong>Опис: </strong>{{ post.body }}</div>
-    </div>
+  <div class="app">
+    <h1>Сторінка з постами</h1>
+    <my-button @click="showDialog" style="margin: 15px 0"
+      >Створити користувача</my-button
+    >
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+
+    <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+
 export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+
   data() {
     return {
       posts: [
         { id: 1, title: "Javascript", body: "Опис поста" },
         { id: 2, title: "Javascript 2", body: "Опис поста 2" },
         { id: 3, title: "Javascript 3", body: "Опис поста 3" },
+        { id: 4, title: "Javascript 4", body: "Опис поста 4" },
       ],
+
+      dialogVisible: false,
     };
   },
-  methods: {},
+
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+  },
 };
 </script>
 
@@ -29,9 +57,7 @@ export default {
   box-sizing: border-box;
 }
 
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
+.app {
+  padding: 20px;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent>
+    <h4>Створення поста</h4>
+    <my-input v-model="post.title" type="text" placeholder="Назва" />
+    <my-input v-model="post.body" type="text" placeholder="Опис" />
+    <my-button
+      class="btn"
+      @click="createPost"
+      style="align-self: flex-end; margin-top: 15px"
+      >Створити</my-button
+    >
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["create"],
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scope>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
